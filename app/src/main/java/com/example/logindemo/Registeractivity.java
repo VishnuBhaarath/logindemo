876541669d4ce56861fd1c8236dd01bd1227aa54
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-03-23 20:26:37 +0530

    Adding authentication for mail and password

--- app/src/main/java/com/example/logindemo/Registeractivity.java
+++ app/src/main/java/com/example/logindemo/Registeractivity.java
@@ -8,23 +8,46 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class  Registeractivity extends AppCompatActivity {
  EditText useremail,userpassword,userconfirmpassword,username;
   Button register;
   TextView uslogin;
+  private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registeractivity);
         SetUpViews();
+        firebaseAuth=FirebaseAuth.getInstance();
         register=(Button)findViewById(R.id.btregister);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(validate()){
+                  String email=useremail.getText().toString().trim();
+                  String password=userpassword.getText().toString().trim();
+                  firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                      @Override
+                      public void onComplete(@NonNull Task<AuthResult> task) {
+                          if(task.isSuccessful()){
+
+                              Toast.makeText(Registeractivity.this,"Registration successfull",Toast.LENGTH_SHORT).show();
+                              startActivity(new Intent(Registeractivity.this,MainActivity.class));
+                          }
+                          else{
+                              Toast.makeText(Registeractivity.this,"Not successfull",Toast.LENGTH_SHORT).show();
+                          }
+                      }
+                  });
 
                 }
             }
@@ -55,7 +78,7 @@ public class  Registeractivity extends AppCompatActivity {
             Toast.makeText(Registeractivity.this,"Registration failed,please enter all the details",Toast.LENGTH_SHORT).show();
         }
         else{
-            Toast.makeText(Registeractivity.this,"Registration Successfull",Toast.LENGTH_SHORT).show();
+           
             result=true;
 
         }
